fdb0b036329db32ad296505213f6dc4c74187eb5
YHYAHIA <yhyahia@gmail.com>
2024-12-05 12:50:46 +0200

    update

---
+++ interactable.gd
@@ -0,0 +1,12 @@
+class_name interactable
+
+extends Area2D
+
+
+func interact(user: Node2D):
+	
+	push_warning("not_implemented")
+	
+	
+func stop_interaction(user : Node2D):
+	push_warning("not_implemented")
