3606f13d19d6e8916e8a4bd1a1afe227db8fd891
YHYAHIA <yhyahia@gmail.com>
2024-12-05 16:49:25 +0200

    Add files via upload

---
+++ interactable.gd
@@ -0,0 +1,11 @@
+class_name interactable
+
+extends Area2D
+@export var key_node :Node2D
+
+func interact(user: Node2D):
+	push_warning("not_implemented")
+	
+	
+func stop_interaction(user : Node2D):
+	push_warning("not_implemented")
